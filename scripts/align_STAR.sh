bf014886e97f9c0083de029916fb9f6e9259a6f6
[ Sarah Walls - Unknown - Graduate Student - mlynch11 ] <scwalls@asu.edu>
2021-04-07 09:59:33 -0700

    Updated STAR and fastqc batch files

--- scripts/align_STAR.sh
+++ scripts/align_STAR.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-#PBS -N PP_RNAseq_STAR_align_T502
-#PBS -k o
-#PBS -l nodes=1:ppn=16,vmem=80gb
-#PBS -l walltime=4:00:00
-#PBS -m abe
+#SBATCH -c 1                        
+#SBATCH -t 01-6:00
+#SBATCH -o %j.OUT
+#SBATCH -e %j.ERROR
 
-module load star
+module load star/2.4.2a
 
-fileDir=/N/dc2/scratch/rtraborn/T502_fastqs/PP_RNAseq
+fileDir=/data/LynchLabCME/Daphnia/DaphniaDevel/RNAseq
 ####### Before running the script, please enter path to desired output directory, below ####
-WD=/N/dc2/scratch/rtraborn/T502_RNAseq/
+WD=/scratch/scwalls/DaphniaDevel/STAR_RNA
 fqDir=fastqs
-genomedir=${WD}/fasta
-genomeFasta=pacificus_Hybrid2.fa
-configfile=${WD}/scripts/STARalign.conf
+genomedir=/scratch/scwalls/STRIPEseq_pipelines/GoSTRIPES_sing/STRIPES/DpGENOME
+genomeFasta=PA42.4.1.fasta
+configfile=/scratch/scwalls/T502_RNAseq/scripts/STARalign.conf
 nThreads=16
 
 
@@ -43,27 +42,56 @@ if [ ! -d "$fqDir" ] ; then
 
 cd $fqDir
 
-ln -s ${fileDir}/GSF1659-NHR40-1_S5_R1_001.fastq.gz NHR40-1.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-1_S5_R2_001.fastq.gz NHR40-1.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-2_S6_R1_001.fastq.gz NHR40-2.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-2_S6_R2_001.fastq.gz NHR40-2.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-3_S7_R1_001.fastq.gz NHR40-3.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-3_S7_R2_001.fastq.gz NHR40-3.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-4_S8_R1_001.fastq.gz NHR40-4.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-4_S8_R2_001.fastq.gz NHR40-4.R2.fastq.gz
-
-ln -s ${fileDir}/GSF1659-Seud1-1_S1_R1_001.fastq.gz Seud1-1.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-1_S1_R2_001.fastq.gz Seud1-1.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-2_S2_R1_001.fastq.gz Seud1-2.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-2_S2_R2_001.fastq.gz Seud1-2.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-3_S3_R1_001.fastq.gz Seud1-3.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-3_S3_R2_001.fastq.gz Seud1-3.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-4_S4_R1_001.fastq.gz Seud1-4.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-4_S4_R2_001.fastq.gz Seud1-4.R2.fastq.gz
+ln -s ${fileDir}/GSF2805-E1_S5_R1_001.fastq.gz E1_S5_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-E1_S5_R2_001.fastq.gz E1_S5_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-E0_S2_R1_001.fastq.gz E0_S2_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-E0_S2_R2_001.fastq.gz E0_S2_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-D1_S4_R1_001.fastq.gz D1_S4_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-D1_S4_R2_001.fastq.gz D1_S4_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-D0_S1_R1_001.fastq.gz D0_S1_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-D0_S1_R2_001.fastq.gz D0_S1_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-C2_S6_R1_001.fastq.gz C2_S6_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-C2_S6_R2_001.fastq.gz C2_S6_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-B1_S3_R1_001.fastq.gz B1_S3_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-B1_S3_R2_001.fastq.gz B1_S3_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-F9_S17_R1_001.fastq.gz F9_S17_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-F9_S17_R2_001.fastq.gz F9_S17_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-F3_S9_R1_001.fastq.gz F3_S9_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-F3_S9_R2_001.fastq.gz F3_S9_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-F10_S20_R1_001.fastq.gz F10_S20_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-F10_S20_R2_001.fastq.gz F10_S20_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-E2_S7_R1_001.fastq.gz E2_S7_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-E2_S7_R2_001.fastq.gz E2_S7_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-D5_S11_R1_001.fastq.gz D5_S11_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-D5_S11_R2_001.fastq.gz D5_S11_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-D4_S10_R1_001.fastq.gz D4_S10_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-D4_S10_R2_001.fastq.gz D4_S10_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-D3_S8_R1_001.fastq.gz D3_S8_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-D3_S8_R2_001.fastq.gz D3_S8_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-C7_S14_R1_001.fastq.gz C7_S14_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-C7_S14_R2_001.fastq.gz C7_S14_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-B9_S16_R1_001.fastq.gz B9_S16_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-B9_S16_R2_001.fastq.gz B9_S16_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-B7_S13_R1_001.fastq.gz B7_S13_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-B7_S13_R2_001.fastq.gz B7_S13_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-B11_S22_R1_001.fastq.gz B11_S22_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-B11_S22_R2_001.fastq.gz B11_S22_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-B10_S19_R1_001.fastq.gz B10_S19_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-B10_S19_R2_001.fastq.gz B10_S19_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-A9_S15_R1_001.fastq.gz A9_S15_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-A9_S15_R2_001.fastq.gz A9_S15_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-A7_S12_R1_001.fastq.gz A7_S12_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-A7_S12_R2_001.fastq.gz A7_S12_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-A1_S23_R1_001.fastq.gz A1_S23_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-A1_S23_R2_001.fastq.gz A1_S23_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-A11_S21_R1_001.fastq.gz A11_S21_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-A11_S21_R2_001.fastq.gz A11_S21_R2.fastq.gz
+ln -s ${fileDir}/GSF2805-A10_S18_R1_001.fastq.gz A10_S18_R1.fastq.gz
+ln -s ${fileDir}/GSF2805-A10_S18_R2_001.fastq.gz A10_S18_R2.fastq.gz
 
 cd $WD
 
-echo "Retrieving Pristionchus genome assembly and annotation files"
+echo "Retrieving Daphnia  genome assembly and annotation files"
 
 #source 0README
 
@@ -81,8 +109,8 @@ cd $genomedir
   fi
 
   cd ${WD}/${fqDir}
-  for file1 in *.R1.fastq.gz; do
-    file2=$(basename $file1 .R1.fastq.gz).R2.fastq.gz
+  for file1 in *_R1.fastq.gz; do
+    file2=$(basename $file1 _R1.fastq.gz)_R2.fastq.gz
     echo "STAR --runMode alignReads --runThreadN $numproc  ${STARalignReadsOptions}  --outSAMtype BAM SortedByCoordinate --outSAMorder Paired  --outFileNamePrefix $(basename $file1 _001.fastq.gz).STAR.  --genomeDir $genomedir  --readFilesIn ${file1} ${file2}"
     STAR --runMode alignReads --runThreadN $nThreads  ${STARalignReadsOptions}  --outSAMtype BAM SortedByCoordinate --outSAMorder Paired  --outFileNamePrefix $(basename $file1 .fastq).STAR. --genomeDir $genomedir  --readFilesIn ${file1} ${file2}
   done
--- scripts/fastqc_batch.sh
+++ scripts/fastqc_batch.sh
@@ -1,44 +1,73 @@
 #!/bin/bash
 
-#PBS -N PP_RNAseq_fastqc_batch_T502
-#PBS -k o
-#PBS -l nodes=1:ppn=16,vmem=32gb
-#PBS -l walltime=1:00:00
-#PBS -m abe
+#SBATCH -c 1
+#SBATCH -t 00-24:00
+#SBATCH -o %j.OUT
+#SBATCH -e %j.ERROR
 
-module load fastqc
+module load fastqc/0.11.7
 
-fileDir=/N/dc2/scratch/rtraborn/T502_fastqs/PP_RNAseq
+fileDir=/data/LynchLabCME/Daphnia/DaphniaDevel/RNAseq
 ####### Before running the script, please enter path to desired output directory, below ####
-fqDir=<provide path to desired output directory>
+fqDir=/scratch/scwalls/DaphniaDevel/real_RNAseq
+
+echo "Creating links to RNA files."
 
 cd $fqDir
 
-ln -s ${fileDir}/GSF1659-NHR40-1_S5_R1_001.fastq.gz NHR40-1.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-1_S5_R2_001.fastq.gz NHR40-1.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-2_S6_R1_001.fastq.gz NHR40-2.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-2_S6_R2_001.fastq.gz NHR40-2.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-3_S7_R1_001.fastq.gz NHR40-3.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-3_S7_R2_001.fastq.gz NHR40-3.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-4_S8_R1_001.fastq.gz NHR40-4.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-4_S8_R2_001.fastq.gz NHR40-4.R2.fastq.gz
-
-ln -s ${fileDir}/GSF1659-Seud1-1_S1_R1_001.fastq.gz Seud1-1.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-1_S1_R2_001.fastq.gz Seud1-1.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-2_S2_R1_001.fastq.gz Seud1-2.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-2_S2_R2_001.fastq.gz Sedu1-2.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-3_S3_R1_001.fastq.gz Seud1-3.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-3_S3_R2_001.fastq.gz Seud1-3.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-4_S4_R1_001.fastq.gz Seud1-4.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-4_S4_R2_001.fastq.gz Seud1-4.R2.fastq.gz
+ln -s ${fileDir}/GSF2805-E1_S5_R1_001.fastq.gz E1_S5_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-E1_S5_R2_001.fastq.gz E1_S5_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-E0_S2_R1_001.fastq.gz E0_S2_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-E0_S2_R2_001.fastq.gz E0_S2_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-D1_S4_R1_001.fastq.gz D1_S4_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-D1_S4_R2_001.fastq.gz D1_S4_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-D0_S1_R1_001.fastq.gz D0_S1_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-D0_S1_R2_001.fastq.gz D0_S1_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-C2_S6_R1_001.fastq.gz C2_S6_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-C2_S6_R2_001.fastq.gz C2_S6_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-B1_S3_R1_001.fastq.gz B1_S3_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-B1_S3_R2_001.fastq.gz B1_S3_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-F9_S17_R1_001.fastq.gz F9_S17_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-F9_S17_R2_001.fastq.gz F9_S17_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-F3_S9_R1_001.fastq.gz F3_S9_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-F3_S9_R2_001.fastq.gz F3_S9_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-F10_S20_R1_001.fastq.gz F10_S20_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-F10_S20_R2_001.fastq.gz F10_S20_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-E2_S7_R1_001.fastq.gz E2_S7_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-E2_S7_R2_001.fastq.gz E2_S7_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-D5_S11_R1_001.fastq.gz D5_S11_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-D5_S11_R2_001.fastq.gz D5_S11_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-D4_S10_R1_001.fastq.gz D4_S10_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-D4_S10_R2_001.fastq.gz D4_S10_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-D3_S8_R1_001.fastq.gz D3_S8_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-D3_S8_R2_001.fastq.gz D3_S8_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-C7_S14_R1_001.fastq.gz C7_S14_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-C7_S14_R2_001.fastq.gz C7_S14_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-B9_S16_R1_001.fastq.gz B9_S16_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-B9_S16_R2_001.fastq.gz B9_S16_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-B7_S13_R1_001.fastq.gz B7_S13_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-B7_S13_R2_001.fastq.gz B7_S13_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-B11_S22_R1_001.fastq.gz B11_S22_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-B11_S22_R2_001.fastq.gz B11_S22_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-B10_S19_R1_001.fastq.gz B10_S19_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-B10_S19_R2_001.fastq.gz B10_S19_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-A9_S15_R1_001.fastq.gz A9_S15_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-A9_S15_R2_001.fastq.gz A9_S15_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-A7_S12_R1_001.fastq.gz A7_S12_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-A7_S12_R2_001.fastq.gz A7_S12_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-A1_S23_R1_001.fastq.gz A1_S23_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-A1_S23_R2_001.fastq.gz A1_S23_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-A11_S21_R1_001.fastq.gz A11_S21_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-A11_S21_R2_001.fastq.gz A11_S21_R2_001.fastq.gz
+ln -s ${fileDir}/GSF2805-A10_S18_R1_001.fastq.gz A10_S18_R1_001.fastq.gz
+ln -s ${fileDir}/GSF2805-A10_S18_R2_001.fastq.gz A10_S18_R2_001.fastq.gz
 
 echo "Starting job"
 
 echo "Running fastqc on the RNA-seq files"
 
 for fq in *.fastq.gz; do
-echo "Starting fastqc for $fq"
-fastqc $fq
+	fastqc $fq
 done
 
 echo "Fastqc job is complete"
